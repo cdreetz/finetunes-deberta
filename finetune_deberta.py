16804126488a1114bbc08c4c987c6d6336b351e1
christian <cdreetz@gmail.com>
2024-08-22 16:14:21 +0000

    add wandb

--- finetune_deberta.py
+++ finetune_deberta.py
@@ -1,5 +1,6 @@
 import os
 import numpy as np
+import wandb
 from datasets import load_dataset
 from transformers import (
   AutoModelForSequenceClassification,
@@ -9,6 +10,8 @@ from transformers import (
   DataCollatorWithPadding
 )
 from sklearn.metrics import accuracy_score, f1_score
+
+wandb.init(project="deberta-v3-xsmall-mnli")
 
 model_name = "microsoft/deberta-v3-xsmall"
 task = "mnli"
@@ -32,16 +35,19 @@ tokinzed_dataset = dataset.map(preprocess_function, batched=True)
 def compute_metrics(eval_pred):
   predictions, labels = eval_pred
   predictions = np.argmax(predictions, axis=1)
+  accuracy = accuracy_score(labels, predictions)
+  f1 = f1_score(labels, predictions, average="weighted")
+  wandb.log({"accuracy": accuracy, "f1": f1})
   return {
-    "accuracy": accuracy_score(labels, predictions),
-    "f1": f1_score(labels, predictions, average="weighted"),
+    "accuracy": accuracy,
+    "f1": f1,
   }
 
 training_args = TrainingArguments(
   output_dir="./results",
   num_train_epochs=3,
   per_device_train_batch_size=16,
-  per_device_eval_batch_size=64,
+  per_device_eval_batch_size=32,
   warmup_steps=500,
   logging_dir="./logs",
   logging_steps=100,
@@ -49,6 +55,7 @@ training_args = TrainingArguments(
   eval_steps=500,
   save_steps=1000,
   load_best_model_at_end=True,
+  report_to="wandb",
 )
 
 trainer = Trainer(
@@ -66,5 +73,9 @@ trainer.train()
 eval_results = trainer.evaluate()
 print(eval_results)
 
+wandb.log(eval_results)
+
 model.save_pretrained("./fine_tuned_deberta_xsmall_mnli")
 tokenizer.save_pretrained("./fine_tuned_deberta_xsmall_mnli")
+
+wandb.finish()
